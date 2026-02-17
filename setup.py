c135274f54a935b54231d1478eb953ae2db79dbe
Prasoon <pprasoon999@gmail.com>
2026-02-17 12:00:00 +0530

    Setup project structure and configuration

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'ytintel',
+    version= '0.0.0',
+    author= 'Prasoon',
+    author_email= 'pprasoon999@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
